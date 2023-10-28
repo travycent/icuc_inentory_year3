fe37d0c0c9a276a2d391a25c7426abf13e2c1b33
Travy Cent <centtravy@gmail.com>
2023-10-28 10:52:17 +0300

    Added Models

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import EquipmentCategory,Products,Transactions
+#Equipment Admin View 
+class EquipmentCategoryAdmin(admin.ModelAdmin):
+    list_display= ('category_name','date_created',  )#Display Data in A List
+    list_filter=('date_created',)
+    search_fields = ('category_name',)#Add A search Field
+#Products Display Admin
+class ProductsAdmin(admin.ModelAdmin):
+    exclude=('available_stock',)
+    list_display= ('category_id','product_name','unit_price','available_stock','unit_measure','date_updated'  )#Display Data in A List
+    list_filter=('date_updated',)
+    search_fields = ('product_name','category_id')#Add A search Field
+#Transactions Display Admin
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display= ('product_id','transaction_type','stock_taken','transaction_amount','transaction_date' )#Display Data in A List
+    list_filter=('transaction_date',)
+    search_fields = ('product_id','transaction_type')#Add A search Field
+#Register Models
+admin.site.register(EquipmentCategory,EquipmentCategoryAdmin)
+admin.site.register(Products,ProductsAdmin)
+admin.site.register(Transactions,TransactionsAdmin)
--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class EquipmentCategory(models.Model):
+    category_id=models.AutoField(primary_key=True)
+    category_name=models.CharField(unique=True,max_length=45)
+    category_description=models.TextField(blank=True)
+    date_created=models.DateField(auto_now=True)
+    class Meta:
+        verbose_name_plural="Equipment Categories"
+    def __str__(self) :
+        return self.category_name
+
+class Products(models.Model):
+    product_id=models.AutoField(primary_key=True)
+    category_id=models.ForeignKey(EquipmentCategory,on_delete=models.CASCADE)
+    product_name=models.CharField(unique=True,max_length=45)
+    unit_price=models.IntegerField(default=0)
+    sale_price=models.IntegerField(default=0)
+    available_stock=models.IntegerField(default=0)
+    unit_measure=models.CharField(max_length=10)
+    date_updated=models.DateField(auto_now=True)
+    class Meta:
+        verbose_name_plural="Products"
+    def __str__(self) :
+        return self.product_name
+class Transactions(models.Model):
+    transaction_id=models.AutoField(primary_key=True)
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    transaction_type=models.CharField(max_length=10)
+    stock_taken=models.IntegerField(default=0)
+    transaction_amount=models.IntegerField(default=0)
+    transaction_date=models.DateField(auto_now=True)
+    class Meta:
+        verbose_name_plural="Transactions"
